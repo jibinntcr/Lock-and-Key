d92009bfc2bec0c6280ab54a6600184721d1ae98
JIBIN N <113666793+jibinntcr@users.noreply.github.com>
2023-12-25 21:58:19 +0530

    Update lock.dart

--- lock.dart
+++ lock.dart
@@ -1,22 +1,15 @@
 import 'dart:math';
 
 class PasswordManager {
-  // Regular expressions for character types
+  // Regular expressions to check for password requirements
   static final RegExp _lowerCase = RegExp(r'[a-z]');
   static final RegExp _upperCase = RegExp(r'[A-Z]');
   static final RegExp _number = RegExp(r'[0-9]');
   static final RegExp _specialChar = RegExp(r'[!@#\$%^&*()_+[\]{}]');
 
   /// Validates the strength of a password.
-  ///
-  /// This method checks if the [password] meets certain criteria to determine
-  /// its strength. It verifies the presence of at least one lowercase letter,
-  /// one uppercase letter, one number, and one special character. Additionally,
-  /// it requires the password to be at least 8 characters long for "Strong"
-  /// strength, 6 characters for "Intermediate," and any length for "Low."
-  ///
-  /// Returns a string indicating the password strength: "Strong",
-  /// "Intermediate", or "Low" based on the criteria.
+  /// Returns "Strong" if the password meets all requirements,
+  /// "Intermediate" if it meets minimum requirements, and "Low" otherwise.
   static String validatePasswordStrength(String password) {
     bool hasLower = _lowerCase.hasMatch(password);
     bool hasUpper = _upperCase.hasMatch(password);
@@ -34,25 +27,17 @@ class PasswordManager {
     }
   }
 
-  /// Generates a password of the specified [level].
-  ///
-  /// The [level] parameter can be one of three values: "strong", "intermediate",
-  /// or "low". Depending on the chosen level, this method selects the character
-  /// set (lowercase letters, uppercase letters, numbers, and special characters)
-  /// and the length of the generated password.
-  ///
-  /// Returns the generated password as a string.
+  /// Generates a password based on the specified [level].
+  /// Returns a password with the desired level of complexity.
   static String generatePassword({required String level}) {
-    // Character sets for password generation
     const String lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz';
     const String upperCaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     const String numbers = '0123456789';
     const String specialChars = '!@#\$%^&*()_+[]{}';
 
-    String chars = ''; // Initialize characters set
-    int length = 8;   // Default password length
+    String chars = '';
+    int length = 8;
 
-    // Select character set and length based on the specified level
     switch (level) {
       case "strong":
         chars = lowerCaseLetters + upperCaseLetters + numbers + specialChars;
@@ -67,19 +52,18 @@ class PasswordManager {
         length = 6;
         break;
       default:
-        return "Invalid level"; // Return error message for an invalid level
+        return "Invalid level";
     }
 
-    // Generate a password by randomly selecting characters from the set
+    // Generate a random password using characters from 'chars'
     return List.generate(length, (index) => chars[Random().nextInt(chars.length)]).join();
   }
 }
 
 void main() {
-  // Sample password for strength validation
-  String password = "safa@TM";
+  String password = "J1Master@2020!!";
   
-  // Validate the password strength and print the result
+  // Validate password strength and print the result
   print("Password Strength: ${PasswordManager.validatePasswordStrength(password)}");
 
   // Generate a strong password and print it
